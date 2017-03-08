36e6e4e0f748ab07711b07e9a9c5bb30e4b716dd
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-08 23:21:57 +0200

    update the main app.jsx Provider :octocat:

--- app.jsx
+++ app.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import InviteContainer from './containers/invite_container';
+import store from './store/store';
+import "./stylesheets/main.scss";
 
-ReactDOM.render(<div>Hello world</div>,
-  document.getElementById('container'));
+const app = (
+  <Provider store={store}>
+    <InviteContainer />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
