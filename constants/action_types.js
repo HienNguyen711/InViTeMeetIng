ab2a433f35e5c8f57efa1e731c087902883b522a
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-09 00:00:53 +0200

    constants file :octocat:

---
+++ constants/action_types.js
@@ -0,0 +1,15 @@
+const actionTypes = {
+    //get 
+  GetInviteRequested: 'GET_INVITE_REQUESTED',
+  GetInviteRejected: 'GET_INVITE_REJECTED',
+  GetInviteFulfilled: 'GET_INVITE_FULFILLED',
+    //add
+
+  AddToInviteRequested: 'ADD_TO_INVITE_REQUESTED',
+  AddToInviteRejected: 'ADD_TO_INVITE_REJECTED',
+  AddToInviteFulfilled: 'ADD_TO_INVITE_FULFILLED',
+
+  GuestAdded: 'GUEST_ADDED'
+};
+
+export default actionTypes;
